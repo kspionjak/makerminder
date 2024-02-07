650761d766364acf34975451f85c222c645e2f44
kspionjak <kristijan.spionjak@gmail.com>
2024-02-07 16:49:09 +0000

    Cherrypy with yfinance
    Just the output

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
-import random
-import string
+import sys
+import pandas as pd
+import yfinance as yf
 import cherrypy
 
 class StringGenerator(object):
 	@cherrypy.expose
 	def index(self):
-		return """<html>
-			<head></head>
-			<body>
-				<form method="get" action="generate">
-				<input type="text" value="8" name="length" />
-				<button type="submit">Give it now!</button>
-				</form>
-			</body>
-		</html>"""
-
-	@cherrypy.expose
-	def generate(self, length=8):
-		some_string = ''.join(random.sample(string.hexdigits, int(length)))
-		cherrypy.session['mystring'] = some_string
-		return some_string
-
-	@cherrypy.expose
-	def display(self):
-		return cherrypy.session['mystring']
+		stock = yf.Ticker('GOOG')
+		hist = stock.history(period="1mo")
+		js_srt = pd.DataFrame.to_json(hist, orient='records', lines=True)
+		return js_srt
 
 if __name__ == '__main__':
 	conf = {
